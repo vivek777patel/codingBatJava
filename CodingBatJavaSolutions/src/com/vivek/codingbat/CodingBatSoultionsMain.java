0be697ff55cd33263ae6256dc3287ddecfcad52a
vp60132n <vp60132n@DESKTOP-BGBR27L>
2018-02-01 14:32:29 -0500

    Made Some Changes

--- CodingBatJavaSolutions/src/com/vivek/codingbat/CodingBatSoultionsMain.java
+++ CodingBatJavaSolutions/src/com/vivek/codingbat/CodingBatSoultionsMain.java
@@ -5,9 +5,35 @@ import com.vivek.codingbat.ap1.WordsFront;
 public class CodingBatSoultionsMain {
 
 	public static void main(String[] args) {
-		System.out.println("AP1 Solutions :");
-		WordsFront ap = new WordsFront();
-		ap.wordsFront(new String[]{"a", "b", "c", "d"}, 1);
+		Dog aDog = new Dog("Rover");
+		// we pass the object to foo
+		foo(aDog);
+		// aDog variable is still pointing to the "Max" dog when foo(...) returns
+		System.out.println(aDog.getName().equals("Max")); // true, java passes by value
+		System.out.println(aDog.getName().equals("RowlfC")); // false
 	}
 
+	public static void foo(Dog d) {
+		d.setName("Max"); // true
+		// change d inside of foo() to point to a new Dog instance "Fifi"
+		d = new Dog("Fifi");
+		d.setName("RowlfC"); // true
+	}
+	
+}
+
+class Dog{
+	String name;
+	
+	public Dog(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
